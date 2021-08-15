c864b60e3ea5725259b28650d6c8236dd5128da5
Claudio Boldorini Junior <boldorini@gmail.com>
2021-08-15 18:58:35 -0300

    Refatoração para melhores práticas utilizando bean

--- camel-microservice-a/src/main/java/com/camel/udemy/microservices/camelmicroservicea/routes/a/MyFirstTimerRouter.java
+++ camel-microservice-a/src/main/java/com/camel/udemy/microservices/camelmicroservicea/routes/a/MyFirstTimerRouter.java
@@ -1,29 +1,22 @@
 package com.camel.udemy.microservices.camelmicroservicea.routes.a;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.camel.udemy.microservices.camelmicroservicea.beans.CurrentTimeBean;
 
 @Component
 public class MyFirstTimerRouter extends RouteBuilder{
 
+	@Autowired
+	CurrentTimeBean currentTimeBean;
+	
 	@Override
 	public void configure() throws Exception {
-		//This is a route... is what we want to do with the data we're about 
-		//to transfer form one microservice to other.
-		//queue - endpoint
-		//transformation
-		//database - endpoint
-				
-		//timer
-		//transformation
-		//log
-		//2021-08-15 18:14:46.177  INFO 3320 --- [r://first-timer] first-timer: Exchange[ExchangePattern: InOnly, BodyType: null, Body: [Body is null]]
-		//timer and log are keywords. 
+		
 		from("timer:first-timer") //this is a channel
-			//Transforming the Body Message into something eles using the transform() method
-			//.transform().constant("My Constant Message")
-			//.transform().constant("Time now is: " +  LocalDateTime.now())
-			.bean("currentTimeBean")
+			.bean(currentTimeBean)
 		.to("log:first-timer");
 	}
 	
